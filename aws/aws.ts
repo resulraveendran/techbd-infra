80b07ca5f21d67e4d92a96be9e4f2baa97ddb3dc
mdvertola <mdvertola@gmail.com>
2024-05-01 12:01:20 -0400

    adding fhir server

--- aws/aws.ts
+++ aws/aws.ts
@@ -1,9 +1,10 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { SynSftpTBD } from './synthetic.sftp.techbd.org/stack';
 import { SynSftpQE } from './synthetic.sftp.qualifiedentity.org/stack';
-const app = new cdk.App();
+import { SynFhirApiQE } from './synthetic.fhir.api.qualifiedentity.org/stack';
 
+
+const app = new cdk.App();
 new SynSftpTBD(app, 'synthetic-sftp-techbd-org', {});
 new SynSftpQE(app, 'synthetic-sftp-qualifiedentity-org', {});
+new SynFhirApiQE(app, 'synthetic-fhir-api-qualifiedentity-org', {});
---
+++ aws/synthetic.fhir.api.qualifiedentity.org/stack.ts
@@ -0,0 +1,99 @@
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as efs from "aws-cdk-lib/aws-efs";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecrAssets from "aws-cdk-lib/aws-ecr-assets";
+import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns";
+import { ManagedPolicy, PolicyStatement, Effect } from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
+import * as dotenv from "dotenv";
+import path = require("path");
+
+
+export interface SynFhirApiQEProps extends cdk.StackProps { }
+
+export class SynFhirApiQE extends cdk.Stack {
+    constructor(scope: Construct, id: string, props: SynFhirApiQEProps) {
+        super(scope, id, props);
+        // Load environment variables from .env file
+        dotenv.config({ path: "./synthetic.fhir.api.qualifiedentity.org/.env" });
+        const containerBuildArgs = {
+            DEPLOYMENT_DOMAIN: process.env.DEPLOYMENT_DOMAIN || "",
+            REPO_URL: process.env.REPO_URL || "",
+            TAG: process.env.TAG || "",
+            DATE: new Date().toISOString(),
+            SEMAPHORE: process.env.SEMAPHORE || "",
+        }
+        // create the VPC
+        const vpc = new ec2.Vpc(this, "VPC", { maxAzs: 2 });
+
+        // create the ECS cluster
+        const cluster = new ecs.Cluster(this, "Cluster", {
+            vpc: vpc,
+            containerInsights: true,
+        });
+        // create a role for fhir tasks to access the EFS filesystem
+        const fhirTaskRole = new iam.Role(this, "fhirTaskRole", {
+            assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+            managedPolicies: [],
+        });
+
+        // create a security group for the fhir service
+        const fhirSg = new ec2.SecurityGroup(
+            this,
+            "fhirServiceSecurityGroup",
+            {
+                vpc,
+                allowAllOutbound: true,
+            }
+        );
+        // allow inbound traffic to the fhir service from the EFS filesystem
+        fhirSg.addIngressRule(
+            ec2.Peer.anyIpv4(),
+            ec2.Port.tcp(efs.FileSystem.DEFAULT_PORT),
+            "Allow inbound traffic to the fhir service from the EFS filesystem"
+        );
+
+        // Define a Docker image asset for the fhir container
+        const fhirDockerImage = new ecrAssets.DockerImageAsset(
+            this,
+            "fhirImage",
+            {
+                directory: "./synthetic.fhir.api.qualifiedentity.org/containers/fhir/", // Adjust this to the path of your Docker context
+                file: "Dockerfile", // Specify the Dockerfile name
+                buildArgs: containerBuildArgs,
+                platform: ecrAssets.Platform.LINUX_AMD64,
+            }
+        );
+
+        // Create a load-balanced Fargate service and make it public
+        const fhirService =
+            new ecsPatterns.ApplicationLoadBalancedFargateService(
+                this,
+                "fhirService",
+                {
+                    cluster,
+                    desiredCount: 1,
+                    cpu: 2048,
+                    memoryLimitMiB: 4096,
+                    taskImageOptions: {
+                        image: ecs.ContainerImage.fromDockerImageAsset(fhirDockerImage),
+                        enableLogging: true,
+                        containerPort: 8080,
+                        taskRole: fhirTaskRole,
+                    },
+                    publicLoadBalancer: false,
+                    listenerPort: 8080,
+                    healthCheckGracePeriod: cdk.Duration.seconds(300),
+                    securityGroups: [fhirSg],
+                    enableExecuteCommand: true,
+                }
+            );
+
+        // add the health check endpoint to the fhir service
+        fhirService.targetGroup.configureHealthCheck({
+            path: "/metadata",
+        });
+    }
+}
