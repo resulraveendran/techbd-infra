f9438c3f335146d563948ff7cc82be05c8e7c8f4
mdvertola <mdvertola@gmail.com>
2024-05-02 07:44:35 -0400

    adding domain and tls

--- aws/aws.ts
+++ aws/aws.ts
@@ -6,15 +6,27 @@ import { EcsCluster, ecsCluster } from './common/ecs-cluster';
 
 
 const app = new cdk.App();
-new SynSftpTBD(app, 'synthetic-sftp-techbd-org', {});
+new SynSftpTBD(app, 'synthetic-sftp-techbd-org', {
 
+});
 
-const SynQECluster = new EcsCluster(app, 'synthetic-shared-qualifiedentity-org', {});
+const region = "us-east-1";
+const account= "339712786701";
+const SynQECluster = new EcsCluster(app, 'synthetic-shared-qualifiedentity-org', {
+    env: {
+        account,
+        region
+    }
+});
 new SynSftpQE(app, 'synthetic-sftp-qualifiedentity-org', {
     vpc: SynQECluster.vpc,
     cluster: SynQECluster.cluster
 });
 new SynFhirApiQE(app, 'synthetic-fhir-api-qualifiedentity-org', {
+    env: {
+        account,
+        region
+    },
     vpc: SynQECluster.vpc,
     cluster: SynQECluster.cluster,
     cert: SynQECluster.certificate,
